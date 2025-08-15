75ce144227086edb4d7df7ee5a4044de61e0174f
oshadakavinda <oshadakavinda2@gmail.com>
2025-08-15 18:03:17 +0530

    Changes to be committed: 	modified:   .zen/config.yaml 	modified:   model/__pycache__/evaluation.cpython-312.pyc 	modified:   pipelines/__pycache__/training_pipeline.cpython-312.pyc 	modified:   pipelines/training_pipeline.py 	modified:   requirements.txt 	modified:   steps/__pycache__/clean_data.cpython-312.pyc 	modified:   steps/__pycache__/evaluation.cpython-312.pyc

--- pipelines/training_pipeline.py
+++ pipelines/training_pipeline.py
@@ -1,5 +1,5 @@
 from zenml import pipeline
-from steps.clean_data import clean_df
+from steps.clean_data import clean_data
 from steps.evaluation import evaluate_model
 from steps.ingest_data import ingest_data
 from steps.model_train import train_model
@@ -8,7 +8,7 @@ from steps.model_train import train_model
 @pipeline()
 def train_pipeline(data_path: str):
     df = ingest_data(data_path)
-    clean_df(df)
-    train_model(df)
-    evaluate_model(df)
+    x_train, x_test, y_train, y_test = clean_data(df)
+    model = train_model(x_train, x_test, y_train, y_test)
+    mse, rmse = evaluate_model(model, x_test, y_test)
     
